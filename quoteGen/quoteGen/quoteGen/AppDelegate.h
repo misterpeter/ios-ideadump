0c4fb43c8b51b2a2c1aaa976556af610874e7bd1
SPM <misterpeter@gmail.com>
2012-07-18 20:24:18 +0200

    created quoteGen

---
+++ quoteGen/quoteGen/quoteGen/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  quoteGen
+//
+//  Created by mr.peter on 18.07.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ quoteGen/quoteGen/quoteGen/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  quoteGen
+//
+//  Created by mr.peter on 18.07.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (nonatomic, retain) NSArray *myQuotes;
+@property (nonatomic, retain) NSMutableArray *movieQuotes;
+
+@end
